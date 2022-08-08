6415853755e733bf6614979c489401735df876cb
sampath <sampathchodipilli@gmail.com>
2022-08-08 11:56:15 +0530

    Home endpoint update

--- src/main/java/com/tweetapp/controller/UserController.java
+++ src/main/java/com/tweetapp/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.tweetapp.controller;
 
 
+import java.util.Map;
+import java.util.Optional;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +24,8 @@ import com.tweetapp.model.AuthRequest;
 import com.tweetapp.model.AuthResponse;
 import com.tweetapp.model.Response;
 import com.tweetapp.model.User;
+import com.tweetapp.repository.UserRepository;
+import com.tweetapp.service.JwtTokenService;
 import com.tweetapp.service.UserService;
 
 @RestController
@@ -30,12 +36,32 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private JwtTokenService jwtTokenService;
+	
+	@Autowired
+	private UserRepository userRepository;
+	
 	private Logger logger = LogManager.getLogger(UserController.class);
 	
 	@GetMapping("/home")
-	public ResponseEntity<Response> home() {
+	public ResponseEntity<Response> home(@RequestHeader Map<String, String> headers) throws Exception {
 		logger.info("Inside TweetApp() Home method ...");
-		return new ResponseEntity<Response>(new Response(Constants.SUCCESS, Constants.HTTP_OK, "User Authenticated !"), HttpStatus.OK);
+		User user = null;
+		headers.forEach((k,v) -> {
+			logger.info(k," - ",v);
+		});
+		String bearerToken = headers.get("Authorization");
+		String username = jwtTokenService.validateTokenAndGetUsername(bearerToken);
+		try {
+			Optional<User> findByEmail = userRepository.findByEmail(username);
+			if(findByEmail.isPresent()) {
+				user = findByEmail.get();
+			}
+		} catch(Exception e) {
+			throw new Exception("User Not Authenticated !");
+		}
+		return new ResponseEntity<Response>(new Response(Constants.SUCCESS, Constants.HTTP_OK, "User Authenticated !", user), HttpStatus.OK);
 	}
 	
 	@PostMapping("/register")
